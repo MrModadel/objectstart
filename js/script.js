1458b938291f0b90b12425ea8cbbb7a693c800db
MrModadel <timanormu@gmail.com>
2023-02-04 18:12:08 +0500

    news

--- js/script.js
+++ js/script.js
@@ -1,42 +1,45 @@
 let user = {
-   name:"Shavkat",
-   surname:"Normurodov",
-   age:15,
-   gender:"male",
-   address:"Ипак йули",
-   phone:998901111111,
-   passport:{
-      id:"UDLDAkmS",
-      data:"2023",
-      todata:"2033",
+   name: "Shavkat",
+   surname: "Normurodov",
+   age: 15,
+   gender: "male",
+   address: "Ипак йули",
+   phone: 998901111111,
+   passport: {
+      id: "UDLDAkmS",
+      data: "2023",
+      todata: "2033",
    },
-   wives:"Мехрибон",
-   nicname:"ShavkatKill",
+   wives: "Мехрибон",
+   nicname: "ShavkatKill",
 };
 let obj = {
-   pol:19,
-   lop:20,
-   oi:90,
-   ser:"Hoi4",
-   o:true,
-   t:false,
+   pol: 19,
+   lop: 20,
+   oi: 90,
+   ser: "Hoi4",
+   o: true,
+   t: false,
 };
-let newsObj = Object.assign({},user,obj);
+let newsObj = Object.assign({}, user, obj);
 let objKeys = Object.keys(newsObj);
 let objyval = Object.values(newsObj);
 let arrObj = objKeys.concat(objyval);
 // console.log(arrObj ,objKeys,objyval);
 let types = {
-   string:[],
-   number:[],
-   boolean:[],
+   string: [],
+   number: [],
+   boolean: [],
+   object: [],
 };
 let st = arrObj.filter(item => {
-   if(typeof item === "string") {
+   if (typeof item === "string") {
       types.string.push(item)
-   } else if(typeof item === "number"){
+   } else if (typeof item === "object") {
+      types.object.push(item)
+   } else if (typeof item === "number") {
       types.number.push(item)
-   } else if(typeof item === "boolean"){
+   } else if (typeof item === "boolean") {
       types.boolean.push(item)
    }
 });
